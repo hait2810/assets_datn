580cb06b3fcd235ec8aea3b22a2807d22868b3bc
Hai <checked.kenvip10@gmail.com>
2022-10-06 16:47:49 +0700

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var imgmain = document.querySelector(".img_imain")
 subimga.forEach((btn) => {
     btn.addEventListener("click", () => {
         console.log("a")
+        console.log("imgsub", btn.src + '-' + "c" , imgmain);
             subimga.forEach((button) => button.classList.remove('select'))
             btn.classList.add("select")
             imgmain.src = btn.src
