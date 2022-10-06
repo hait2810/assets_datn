e73455226e3c8fa8ba965557aa7f23a9f2e27305
Hai <checked.kenvip10@gmail.com>
2022-10-06 16:44:56 +0700

    update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var subimga = document.querySelectorAll(".product-gallery__thumb");
-
+var imgmain = document.querySelector(".img_imain")
 subimga.forEach((btn) => {
     btn.addEventListener("click", () => {
         console.log("a")
             subimga.forEach((button) => button.classList.remove('select'))
             btn.classList.add("select")
-            
+            imgmain.src = btn.src
     })
 })
